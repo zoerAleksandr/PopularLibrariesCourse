955fb0036612d0b1f251c2e8255865360ff6069c
zoer <zoer.aleksandr@gmail.com>
2022-04-04 11:51:59 +0300

    Сделал layout, подключил ViewBinding

--- app/src/main/java/com/example/popularlibrariescourse/MainActivity.kt
+++ app/src/main/java/com/example/popularlibrariescourse/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.popularlibrariescourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.popularlibrariescourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(this, "Тест", Toast.LENGTH_SHORT).show()
+        }
     }
 }
